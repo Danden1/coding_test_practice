1d8e6df577dd3a1538338b2c70a678f2cabab445
권용하 <yongha.kwon12@gmail.com>
2023-03-03 13:05:23 +0900

    30분. 다익스트라. dict 사용X. 중복 경로 문제 발생

---
+++ acmicpc/DFS_BFS/11779.py
@@ -0,0 +1,57 @@
+import heapq
+import sys
+from collections import deque
+
+n = int(sys.stdin.readline())
+m = int(sys.stdin.readline())
+
+board = [deque() for _ in range(n+1)]
+
+for _ in range(m):
+    a,b,c = map(int, sys.stdin.readline().split())
+    
+    board[a].append([b,c])
+    
+start, end = map(int, sys.stdin.readline().split())
+    
+
+hq = deque()
+
+heapq.heappush(hq, [0, 1])
+
+dk = [[987654321, -1] for i in range(n+1)]
+dk[start][0] = 0
+dk[start][1] = start
+
+while hq:
+    dst, cur_node = heapq.heappop(hq)
+
+    if dk[cur_node][0] < dst:
+        continue
+    
+    for nx_node, nx_d in board[cur_node]:
+        nx_dst = nx_d + dst
+        
+        if dk[nx_node][0] > nx_dst:
+            dk[nx_node][0] = nx_dst
+            dk[nx_node][1] = cur_node
+            
+            
+            heapq.heappush(hq, [nx_dst, nx_node])
+        
+
+cur = end
+
+
+
+ans = deque()
+
+while cur!=start:
+    ans.appendleft(cur)
+    cur=dk[cur][1]
+
+ans.appendleft(start)
+
+print(dk[end][0])
+print(len(ans))
+print(' '.join(map(str, ans)))
